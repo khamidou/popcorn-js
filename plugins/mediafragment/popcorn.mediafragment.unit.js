61af8e24b784d7c0f63f0f947b2023008c7ab73d
Karim Hamidou <khamidou@gmail.com>
2011-11-17 11:24:59 +0100

    indentation and style fixes

--- plugins/mediafragment/popcorn.mediafragment.unit.js
+++ plugins/mediafragment/popcorn.mediafragment.unit.js
@@ -2,7 +2,7 @@ test( "Popcorn Mediafragment Plugin", function() {
 
   var popped = Popcorn( "#video" ),
       expects = 4,
-      count = 0;     
+      count = 0;
 
   expect( expects );
 
@@ -17,48 +17,46 @@ test( "Popcorn Mediafragment Plugin", function() {
   ok( "mediafragment" in popped, "mediafragment is a method of the popped instance" );
   plus();
 
-  var currstate = window.location.href.split("#t=");
+  var currstate = window.location.href.split( "#t=" );
   equals( currstate[1], undefined, "initially, there is no # in the url" );
   plus();
 
   popped.mediafragment({
-    start: 0,   
+    start: 0,
   });
 
   popped.exec( 2, function() {
     popped.pause();
     popped.play();
-    
-    
   });
 
   popped.exec( 3, function() {
-    var currstate = window.location.href.split("#t=");
-    var time = + currstate[1];
-    
+    var currstate = window.location.href.split( "#t=" );
+    var time = + currstate[ 1 ];
+
     // popcorn's precision is limited to the second.
     ok(time >= 2 && time <= 3, "correct time is set" );
     plus();
     popped.play();
-  
+
     popped.currentTime(5);
   });
 
-  popped.exec( 6, function() {      
-    var currstate = window.location.href.split("#t=");
-    var time = + currstate[1];
+  popped.exec( 6, function() {
+    var currstate = window.location.href.split( "#t=" );
+    var time = + currstate[ 1 ];
 
     console.log(time);
     // popcorn's precision is limited to the second.
     ok(time >= 5 && time <= 6, "correct time is set after seeking" );
-    plus();   
-  
+    plus();
+
   });
-  
+
   popped.exec( 10, function() {
     // clear the address bar
-    window.location.href = window.location.href.split("#t=")[0];
+    window.location.href = window.location.href.split( "#t=" )[ 0 ];
   });
-  
+
   popped.play();
 });
